426814f59283037a7f609a486896a7cbe2f03033
Juan Carlos Narvaez <jcnarvaeziturbe@hotmail.com>
2025-02-15 19:27:23 -0400

    Added some exception handling and fixed some errors
    The exception handling is very basic and could be done better but not really needed. The errors I think are finally done but need to double check. Database should be ready for stress testing

--- Python/database.py
+++ Python/database.py
@@ -22,8 +22,8 @@ def create_tables(connection):
     :param connection: Connection to the database.
     :return: None
     """
-    #So .cursor() can be skipped? I do need to test it and read more about it
-    connection.executescript(""" 
+    cursor = connection.cursor()
+    cursor.executescript(""" 
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
@@ -42,7 +42,7 @@ def create_tables(connection):
             exercise_id INTEGER NOT NULL,
             grade REAL NOT NULL,
             time_attempt TEXT DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (user_id) REFERENCES users(id)
+            FOREIGN KEY (user_id) REFERENCES users(id),
             FOREIGN KEY (exercise_id) REFERENCES exercises(id)
         );
         
@@ -52,12 +52,13 @@ def create_tables(connection):
             average_grade REAL DEFAULT 0,
             attempt_count INTEGER DEFAULT 0,
             last_attempt TEXT DEFAULT CURRENT_TIMESTAMP,
-            PRIMARY KEY (user_id, exercise_id).
+            PRIMARY KEY (user_id, exercise_id),
             FOREIGN KEY (user_id) REFERENCES users(id),
             FOREIGN KEY (exercise_id) REFERENCES exercises(id)
         );
     """)
     connection.commit()
+    cursor.close()
 
 #Insertion operations
 def add_user(connection, name):
@@ -67,12 +68,18 @@ def add_user(connection, name):
     :param name: Name of the user. (string)
     :return: None
     """
-    cursor = connection.cursor()
-    #So datetime.utcnow is going to be removed at some point so better to use the following code
-    date = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
-    cursor.execute("INSERT INTO users (name, created_at) VALUES (?, ?)",
-                   (name, date))
-    connection.commit()
+    try:
+        cursor = connection.cursor()
+        #So datetime.utcnow is going to be removed at some point so better to use the following code
+        date = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+        cursor.execute("INSERT INTO users (name, created_at) VALUES (?, ?)",
+                       (name, date))
+        connection.commit()
+    except sqlite3.Error as err:
+        print(f"Database Error: {err}")
+    finally:
+        #Cursors need to be closed, breaking news
+        cursor.close()
 
 def add_exercise(connection, name, description):
     """
@@ -82,63 +89,95 @@ def add_exercise(connection, name, description):
     :param description: Description of the exercise. (string)
     :return: None
     """
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO exercises (name, description) VALUES (?, ?)",
-                   (name, description))
-    connection.commit()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO exercises (name, description) VALUES (?, ?)",
+                       (name, description))
+        connection.commit()
+    except sqlite3.Error as err:
+        print(f"Database Error: {err}")
+    finally:
+        cursor.close()
 
 def add_grade(connection, user_id, exercise_id, grade):
     """
-    Add a new grade as a result of an exercise for the desired user.
+    Add a new grade as a result of an exercise for the desired user and updates the user progression.
     :param connection: Connection to the database.
     :param user_id: User id of the owner of the grade. (integer)
     :param exercise_id: Exercise id of the exercise attempted. (integer)
     :param grade: Grade obtained by the user. (float)
     :return: None
     """
-    cursor = connection.cursor()
-    date = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
-    cursor.execute("INSERT INTO grades (user_id, exercise_id, grade, time_attempt) VALUES (?, ?, ?, ?)",
-                   (user_id, exercise_id, grade, date))
-    connection.commit()
+    try:
+        cursor = connection.cursor()
+        date = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+        cursor.execute("INSERT INTO grades (user_id, exercise_id, grade, time_attempt) VALUES (?, ?, ?, ?)",
+                    (user_id, exercise_id, grade, date))
+        connection.commit()
+        update_user_progression(connection, user_id, exercise_id, grade)
+    except sqlite3.Error as err:
+        print(f"Database Error: {err}")
+    finally:
+        cursor.close()
 
 #SELECT operations
 def get_users(connection):
     """
     Get all users from the database.
     :param connection: Connection to the database.
-    :return: List containing the rows of the users table.
+    :return: List containing the rows of the users table, otherwise an empty list if an error
+    is encountered.
     """
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM users")
-    return cursor.fetchall()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users")
+        return cursor.fetchall()
+    except sqlite3.Error as err:
+        print(f"Error getting the users: {err}")
+        return []
+    finally:
+        cursor.close()
 
 def get_exercises(connection):
     """
     Get all exercises from the database.
     :param connection: Connection to the database.
-    :return: List containing the rows of the exercises table.
+    :return: List containing the rows of the exercises table, otherwise an empty list if an error
+    is encountered.
     """
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM exercises")
-    return cursor.fetchall()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM exercises")
+        return cursor.fetchall()
+    except sqlite3.Error as err:
+        print(f"Error getting the exercises: {err}")
+        return []
+    finally:
+        cursor.close()
 
 def get_grades_of_user(connection, user_id):
     """
     Get all the grades of the specified user.
     :param connection: Connection to the database.
-    :param user_id: User id of the user which the scores are tied to. (integer)
-    :return: List containing the scores of the specified user.
+    :param user_id: User id of the user which the grades are tied to. (integer)
+    :return: List containing the grades of the specified user, otherwise an empty list if an error
+    is encountered.
     """
-    cursor = connection.cursor()
-    cursor.execute("""
-        SELECT exercises.name, grades.grade, grades.time_attempt
-        FROM grades
-        JOIN exercises ON grades.exercise_id = exercises.id
-        WHERE grades.user_id = ?
-        ORDER BY grades.time_attempt DESC
-    """, (user_id,))
-    return cursor.fetchall()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("""
+            SELECT exercises.name, grades.grade, grades.time_attempt
+            FROM grades
+            JOIN exercises ON grades.exercise_id = exercises.id
+            WHERE grades.user_id = ?
+            ORDER BY grades.time_attempt DESC
+        """, (user_id,))
+        return cursor.fetchall()
+    except sqlite3.Error as err:
+        print(f"Error getting the grades of the user: {err}")
+        return []
+    finally:
+        cursor.close()
 
 def get_user_progress(connection, user_id):
     """
@@ -147,10 +186,16 @@ def get_user_progress(connection, user_id):
     :param user_id: User id of the user progress being read. (integer)
     :return: Tuple containing the user progress including average grade, number of attempts and date of last attempt.
     """
-    cursor = connection.cursor()
-    cursor.execute("SELECT user_id, average_grade, attempt_count, last_attempt FROM user_progress WHERE user_id = ?",
-                   (user_id,))
-    return cursor.fetchone()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT user_id, average_grade, attempt_count, last_attempt FROM user_progress WHERE user_id = ?",
+                       (user_id,))
+        return cursor.fetchone()
+    except sqlite3.Error as err:
+        print(f"Error getting the user progress: {err}")
+        return ()
+    finally:
+        cursor.close()
 
 #Delete operations
 def remove_user(connection, user_id):
@@ -160,10 +205,15 @@ def remove_user(connection, user_id):
     :param user_id: User id of the user to be removed from the database. (integer)
     :return: None
     """
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM grades WHERE user_id = ?", (user_id,))
-    cursor.execute("DELETE FROM users WHERE id = ?", (user_id,))
-    connection.commit()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM grades WHERE user_id = ?", (user_id,))
+        cursor.execute("DELETE FROM users WHERE id = ?", (user_id,))
+        connection.commit()
+    except sqlite3.Error as err:
+        print(f"Error removing user: {err}")
+    finally:
+        cursor.close()
 
 def remove_exercise(connection, exercise_id):
     """
@@ -172,10 +222,15 @@ def remove_exercise(connection, exercise_id):
     :param exercise_id: Exercise id of the exercise to be removed from the database. (integer)
     :return: None
     """
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM grades WHERE exercise_id = ?", (exercise_id,))
-    cursor.execute("DELETE FROM exercises WHERE id = ?", (exercise_id,))
-    connection.commit()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM grades WHERE exercise_id = ?", (exercise_id,))
+        cursor.execute("DELETE FROM exercises WHERE id = ?", (exercise_id,))
+        connection.commit()
+    except sqlite3.Error as err:
+        print(f"Error removing exercise: {err}")
+    finally:
+        cursor.close()
 
 def remove_grade(connection, grade_id):
     """
@@ -184,9 +239,14 @@ def remove_grade(connection, grade_id):
     :param grade_id: Grade id of the grade to be removed from the database. (integer)
     :return: None
     """
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM grades WHERE id = ?", (grade_id,))
-    connection.commit()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM grades WHERE id = ?", (grade_id,))
+        connection.commit()
+    except sqlite3.Error as err:
+        print(f"Error removing grade: {err}")
+    finally:
+        cursor.close()
 
 def remove_user_progress(connection, user_id):
     """
@@ -195,9 +255,14 @@ def remove_user_progress(connection, user_id):
     :param user_id: User ID whose progress is being deleted. (integer)
     :return: None
     """
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM user_progress WHERE user_id = ?", (user_id,))
-    connection.commit()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM user_progress WHERE user_id = ?", (user_id,))
+        connection.commit()
+    except sqlite3.Error as err:
+        print(f"Error removing user progress: {err}")
+    finally:
+        cursor.close()
 
 #Update operations
 def update_user(connection, name, user_id):
@@ -208,9 +273,14 @@ def update_user(connection, name, user_id):
     :param user_id: ID of the user which will change the name. (integer)
     :return: None
     """
-    cursor = connection.cursor()
-    cursor.execute("UPDATE users SET name = ? WHERE id = ?", (name, user_id))
-    connection.commit()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("UPDATE users SET name = ? WHERE id = ?", (name, user_id))
+        connection.commit()
+    except sqlite3.Error as err:
+        print(f"Error updating user: {err}")
+    finally:
+        cursor.close()
 
 def update_exercise(connection, exercise_id, name = None, description = None):
     """
@@ -221,13 +291,18 @@ def update_exercise(connection, exercise_id, name = None, description = None):
     :param description: New description of the exercise, default None. (string)
     :return: None
     """
-    cursor = connection.cursor()
-    #I do want to revisit this to reduce number of checks if possible!
-    if name:
-        cursor.execute("UPDATE exercise SET name = ? WHERE id = ?", (name, exercise_id))
-    elif description:
-        cursor.execute("UPDATE exercise SET description = ? WHERE id = ?", (description, exercise_id))
-    connection.commit()
+    try:
+        cursor = connection.cursor()
+        #It can be done better but for now this works
+        if name:
+            cursor.execute("UPDATE exercise SET name = ? WHERE id = ?", (name, exercise_id))
+        if description:
+            cursor.execute("UPDATE exercise SET description = ? WHERE id = ?", (description, exercise_id))
+        connection.commit()
+    except sqlite3.Error as err:
+        print(f"Error updating exercise: {err}")
+    finally:
+        cursor.close()
 
 #So this function may not really need to even exist, but I will leave it just in case
 def update_grades(connection, grade_id, grade):
@@ -238,9 +313,14 @@ def update_grades(connection, grade_id, grade):
     :param grade: New grade to be added. (float)
     :return: None
     """
-    cursor = connection.cursor()
-    cursor.execute("UPDATE grades SET name = ? WHERE id = ?", (grade, grade_id))
-    connection.commit()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("UPDATE grades SET grade = ? WHERE id = ?", (grade, grade_id))
+        connection.commit()
+    except sqlite3.Error as err:
+        print(f"Error updating grade: {err}")
+    finally:
+        cursor.close()
 
 def update_user_progression(connection, user_id, exercise_id, new_grade):
     """
@@ -251,22 +331,26 @@ def update_user_progression(connection, user_id, exercise_id, new_grade):
     :param new_grade: New grade to be added. (float)
     :return: None
     """
-    cursor = connection.cursor()
-    cursor.execute("SELECT average_grade, attempt_count FROM user_progress WHERE user_id = ? AND exercise_id = ?",
-                   (user_id, exercise_id))
-    current_progress = cursor.fetchone()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT average_grade, attempt_count FROM user_progress WHERE user_id = ? AND exercise_id = ?",
+                    (user_id, exercise_id))
+        current_progress = cursor.fetchone()
 
-    new_average, new_attempts = calculate_progress(current_progress, new_grade)
-    date = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+        new_average, new_attempts = calculate_progress(current_progress, new_grade)
+        date = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
 
-    if current_progress:
-        cursor.execute("UPDATE user_progress SET average_grade = ?, attempt_count = ?, last_attempt = ? WHERE user_id = ? AND exercise_id ?",
-                       (new_average, new_attempts, date, user_id, exercise_id))
-    else:
-        cursor.execute("INSERT INTO user_progress (user_id, exercise_id, average_grade, attempt_count, last_attempt) VALUES (?, ?, ?, ?, ?)",
-                       (user_id, exercise_id, new_average, new_attempts, date))
-    connection.commit()
-
+        if current_progress:
+            cursor.execute("UPDATE user_progress SET average_grade = ?, attempt_count = ?, last_attempt = ? WHERE user_id = ? AND exercise_id = ?",
+                        (new_average, new_attempts, date, user_id, exercise_id))
+        else:
+            cursor.execute("INSERT INTO user_progress (user_id, exercise_id, average_grade, attempt_count, last_attempt) VALUES (?, ?, ?, ?, ?)",
+                        (user_id, exercise_id, new_average, new_attempts, date))
+        connection.commit()
+    except sqlite3.Error as err:
+        print(f"Error updating user progress: {err}")
+    finally:
+        cursor.close()
 
 #Extra functions
 def calculate_progress(progress, new_grade):
@@ -296,7 +380,7 @@ def drop_all_tables(connection):
     """)
     connection.commit()
 
-#Testing
+#Simple Testing
 if __name__ == '__main__':
     #Creation
     conn = create_connection(DB_NAME)
@@ -305,24 +389,30 @@ if __name__ == '__main__':
     #Adding
     add_user(conn, "John")
     add_exercise(conn, "Test", "Testing insertion")
-
     add_grade(conn, 1, 1, 59)
+    add_grade(conn, 1, 1, 54)
+    add_grade(conn, 1, 1, 87)
+    add_grade(conn, 1, 1, 45)
 
     #Print Results
     print("Users: ", get_users(conn))
     print("Exercise: ", get_exercises(conn))
     print("Grade for user 1: ", get_grades_of_user(conn, 1))
+    print("User progression: ", get_user_progress(conn, 1))
 
     #Remove
     remove_grade(conn, 1)
-    remove_exercise(conn, 3)
-    remove_user(conn, 4)
+    remove_user_progress(conn, 1)
+    remove_exercise(conn, 1)
+    remove_user(conn, 1)
+
 
     #Print Results
     print("Users: ", get_users(conn))
     print("Exercise: ", get_exercises(conn))
     print("Grade for user 1: ", get_grades_of_user(conn, 1))
+    print("User progression: ", get_user_progress(conn, 1))
 
-    # drop_all_tables(conn)
+
 
     conn.close()
