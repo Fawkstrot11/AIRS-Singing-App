2d5c816a34b26c9ae84dbb85fc8ce057936ecb71
Fawkstrot <113630709+Fawkstrot11@users.noreply.github.com>
2025-04-09 11:19:05 -0300

    Delete __init__.py

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from routes import init_routes
-
-def create_app():
-    app = Flask(__name__, template_folder="../templates", static_folder="../static")
-
-    init_routes(app)
-
-    return app
